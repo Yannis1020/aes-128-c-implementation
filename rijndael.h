79aa8efaaed3bb1c0ace14ce3ca7869fa6e8f992
luoweiting <luoweiting1020@gmail.com>
2025-04-14 15:51:27 +0000

    Update: Implement the function prototypes

--- rijndael.h
+++ rijndael.h
@@ -1,6 +1,9 @@
 /*
- * TODO: Add your name and student number here, along with
- *       a brief description of this code.
+ * Name: Luo Weiting
+ * Student Number: D24125441
+ * Description: This header file defines the interface for an AES-128 (Rijndael) encryption and decryption library.
+ * It provides functions to encrypt and decrypt 128-bit blocks using a 128-bit key, along with the other internal helper functions
+ * for AES transformations and key expansion.
  */
 
  #ifndef RIJNDAEL_H
@@ -8,14 +11,32 @@
  
  #define BLOCK_ACCESS(block, row, col) (block[(row * 4) + col])
  #define BLOCK_SIZE 16
+
  
  /*
-  * These should be the main encrypt/decrypt functions (i.e. the main
-  * entry point to the library for programmes hoping to use it to
-  * encrypt or decrypt data)
+  * Main encrypt/decrypt functions - the entry points to the library
+  * for programs hoping to use it to encrypt or decrypt data
   */
  unsigned char *aes_encrypt_block(unsigned char *plaintext, unsigned char *key);
  unsigned char *aes_decrypt_block(unsigned char *ciphertext, unsigned char *key);
  
+
+/*
+ * Internal helper functions for AES transformations and key expansion.
+ */
+ /* Function prototypes for the core AES operations */
+ void sub_bytes(unsigned char *block);
+ void shift_rows(unsigned char *block);
+ void mix_columns(unsigned char *block);
+ void add_round_key(unsigned char *block, unsigned char *round_key);
+
+ /* Function prototypes for the inverse AES operations */
+ void invert_sub_bytes(unsigned char *block);
+ void invert_shift_rows(unsigned char *block);
+ void invert_mix_columns(unsigned char *block);
+ 
+ /* Key expansion function prototype */
+ unsigned char *expand_key(unsigned char *cipher_key);
+
  #endif
  
